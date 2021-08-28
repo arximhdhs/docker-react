7c2313a3a66e7c8889ab5910411a1d0ac9b8036b
raskolnikof <Ioannis.Paliakis@arhs-hellas-dev.com>
2021-08-28 20:26:38 +0300

    testing deployment

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is the absolute pipeline!!!
         </a>
       </header>
     </div>
